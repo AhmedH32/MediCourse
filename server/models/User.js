7798005b7285981a503985d545825bc8027f3ff9
Ahmed Hassan <ahmed.h.moustafa2003@gmail.com>
2026-04-15 12:12:01 +0200

    refactor: upgrade deviceBinding to structured fingerprintSnapshot schema

--- server/models/User.js
+++ server/models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     name: {
         type: String,
-        required: [true,'Name is required'],
+        required: [true, 'Name is required'],
         trim: true
     },
     role: {
@@ -25,36 +25,60 @@ const userSchema = new mongoose.Schema({
         enum: ['admin', 'student'],
         default: 'student'
     },
-    isDeviceLocked: {
-        type: Boolean,
-        default: false
-    },
-    deviceFingerprint: {
-        type: String,
-        default: null
+    deviceBinding: {
+        deviceUUID: {
+            type: String,
+            default: null
+        },
+        fingerprintSnapshot: {
+            os: { type: String },
+            browser: { type: String },
+            language: { type: String },
+            timeZone: { type: String },
+            resolution: { type: String },
+            hardwareConcurrency: { type: Number },
+            touchSupport: { type: Boolean },
+            canvasHash: { type: String }
+        },
+        boundAt: {
+            type: Date
+        },
+        lastValidatedAt: { type: Date },
+        resetHistory: [
+            {
+                resetAt: { type: Date, default: Date.now },
+                reason: { type: String, default: '' },
+                resetBy: { type: mongoose.Schema.Types.ObjectId, ref:'User'}
+            }
+        ]
     },
     resetToken: {
         type: String,
         default: null
     }
-},{
+}, {
     timestamps: true
 });
 //might add hashing method for the reset token later
 
 
-userSchema.pre('save', async function(next) {
-    if(!this.isModified('password'))
-    {
-        return next();
+userSchema.pre('save', async function(next){
+    try{
+        if(!this.isModified('password')) return next();
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
     }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-});
+    catch(error)
+    {
+        console.log(error);
+        next(error);
+    }
+})
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.matchPassword(enteredPassword, this.password);
-    
+    return await bcrypt.compare(enteredPassword, this.password);
+
 };
 
 module.exports = mongoose.model('User', userSchema);
